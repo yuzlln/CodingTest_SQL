b6fc16465c389226e888835f1206bad8474f23ba
Woo Yujin <biolet2001@gmail.com>
2024-09-08 22:42:18 +0900

    Add files via upload

---
+++ LeetCode/Medium/626. Exchange Seats.sql
@@ -0,0 +1,8 @@
+SELECT CASE WHEN id % 2 = 1 AND id = (SELECT COUNT(*)
+                                      FROM seat
+                                      ) THEN id -- 마지막 값
+            WHEN id % 2 = 0 THEN id - 1 -- 짝수
+            ELSE id + 1 END AS id -- 홀수
+    , student
+FROM seat
+ORDER BY id;
