7574789613fb6cb15d187c9fd0ea0e3572aed979
Woo Yujin <biolet2001@gmail.com>
2024-09-08 10:25:12 +0900

    Add files via upload

---
+++ Programmers/Lv.2/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,15 @@
+-- 방법 1) HAVING절
+SELECT animal_type
+    , COUNT(*) AS count
+FROM animal_ins
+GROUP BY animal_type
+HAVING animal_type IN ('Dog', 'Cat')
+ORDER BY animal_type;
+
+-- 방법 2) WHERE절
+SELECT animal_type
+    , COUNT(*) AS count
+FROM animal_ins
+WHERE animal_type IN ('Dog', 'Cat')
+GROUP BY animal_type
+ORDER BY animal_type;
