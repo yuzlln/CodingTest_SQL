184b08ea0a018a1e8c0562bf171aa2b63e6ba5dd
Woo Yujin <biolet2001@gmail.com>
2024-09-08 13:37:25 +0900

    Add files via upload

---
+++ Programmers/Lv.2/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,12 @@
+SELECT user_id
+    , product_id
+FROM (SELECT user_id
+          , product_id
+          , COUNT(*) AS cnt
+      FROM online_sale
+      GROUP BY user_id
+          , product_id
+     ) AS t
+WHERE cnt >= 2
+ORDER BY user_id
+    , product_id DESC;
