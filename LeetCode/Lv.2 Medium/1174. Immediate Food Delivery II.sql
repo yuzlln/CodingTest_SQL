cace2834f273170ffb0d70c3951302058cf637fa
Woo Yujin <biolet2001@gmail.com>
2024-09-14 20:01:12 +0900

    Update 1174. Immediate Food Delivery II.sql

--- LeetCode/Lv.2 Medium/1174. Immediate Food Delivery II.sql
+++ LeetCode/Lv.2 Medium/1174. Immediate Food Delivery II.sql
@@ -4,18 +4,15 @@ WITH first_order AS (
     FROM delivery
     GROUP BY customer_id
 )
-, first_order_info AS (
+, first_order_immediate_flag AS (
     SELECT *
+        , IF(order_date = customer_pref_delivery_date, 1, 0) AS immediate
     FROM delivery
     WHERE (customer_id, order_date) IN (SELECT *
                                         FROM first_order
                                         )    
 )
-, immediate_flag AS (
-    SELECT *
-        , IF(order_date = customer_pref_delivery_date, 1, 0) AS immediate
-    FROM first_order_info
-)
+
 
 SELECT ROUND(100 * SUM(immediate) / COUNT(*), 2) AS immediate_percentage
-FROM immediate_flag;
+FROM first_order_immediate_flag;
