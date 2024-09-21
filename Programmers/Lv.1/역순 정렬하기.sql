e1c3b45f85eec0acc34a943aa8969d213709286e
Woo Yujin <biolet2001@gmail.com>
2024-09-21 20:06:10 +0900

    Add files via upload

---
+++ Programmers/Lv.1/역순 정렬하기.sql
@@ -0,0 +1,4 @@
+SELECT name
+    , datetime
+FROM animal_ins
+ORDER BY animal_id DESC;
---
+++ Programmers/Lv.1/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -0,0 +1,8 @@
+SELECT dr_name
+    , dr_id
+    , mcdp_cd
+    , DATE_FORMAT(hire_ymd, '%Y-%m-%d') AS hire_ymd
+FROM doctor
+WHERE mcdp_cd IN ('CS', 'GS')
+ORDER BY hire_ymd DESC
+    , dr_name;
