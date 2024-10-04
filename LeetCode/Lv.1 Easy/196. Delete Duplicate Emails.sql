e1b3d321013a21048aeaeece9f7afa1fdf4e6728
Woo Yujin <biolet2001@gmail.com>
2024-10-04 14:59:02 +0900

    Add files via upload

---
+++ LeetCode/Lv.1 Easy/196. Delete Duplicate Emails.sql
@@ -0,0 +1,21 @@
+-- 방법 1) Window Function				
+DELETE FROM person
+WHERE id IN (SELECT id
+             FROM (SELECT *
+                       , ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) AS num
+                   FROM person
+                   ) AS t
+              WHERE num >= 2
+             );
+             
+             
+             
+-- 방법 2) FROM절 서브쿼리
+DELETE FROM person
+WHERE id NOT IN (SELECT sub.min_id
+				 FROM (SELECT email
+					       , MIN(id) as min_id
+					   FROM person
+					   GROUP BY email
+					   ) AS sub
+			    );
