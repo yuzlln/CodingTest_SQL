a7fc7639cf40f1489bdcc51fcac6c9188a871535
Woo Yujin <biolet2001@gmail.com>
2024-10-09 15:27:26 +0900

    Update Weather Observation Station 8.sql

--- HackerRank/Lv.1 Easy/Weather Observation Station 8.sql
+++ HackerRank/Lv.1 Easy/Weather Observation Station 8.sql
@@ -1,4 +1,12 @@
+-- 방법 1) 와일드키
 SELECT DISTINCT city
 FROM station
 WHERE RIGHT(city, 1) IN ('a', 'e', 'i', 'o', 'u')
     AND LEFT(city, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+
+
+-- 방법 2) 정규 표현식
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[aeiou].*[aeiou]$';
