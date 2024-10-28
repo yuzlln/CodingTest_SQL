6ddcf650bd14a1ed39bebb503bc79245b8643098
Woo Yujin <biolet2001@gmail.com>
2024-10-28 18:05:26 +0900

    Add files via upload

---
+++ Programmers/Lv.2/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT board_id
+    , writer_id
+    , title
+    , price
+    , CASE WHEN status = 'SALE' THEN '판매중'
+           WHEN status = 'RESERVED' THEN '예약중'
+           ELSE '거래완료' END AS status
+FROM used_goods_board
+WHERE DATE_FORMAT(created_date, '%Y-%m-%d') = '2022-10-05'
+ORDER BY board_id DESC;
