6e4894cf8de89f555bf80a4669d0e246ba534594
Woo Yujin <biolet2001@gmail.com>
2024-10-29 11:45:24 +0900

    Add files via upload

---
+++ Programmers/Lv.2/ROOT 아이템 구하기.sql
@@ -0,0 +1,8 @@
+SELECT item_id
+    , item_name
+FROM item_info
+WHERE item_id IN (SELECT item_id
+                  FROM item_tree
+                  WHERE parent_item_id IS NULL
+                  )
+ORDER BY item_id;
