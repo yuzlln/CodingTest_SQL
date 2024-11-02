3d87a144727607ea90c3e0b6541f2d01e5779b37
Woo Yujin <biolet2001@gmail.com>
2024-11-02 16:42:44 +0900

    Update 1251. Average Selling Price.sql

--- LeetCode/Lv.1 Easy/1251. Average Selling Price.sql
+++ LeetCode/Lv.1 Easy/1251. Average Selling Price.sql
@@ -3,5 +3,5 @@ SELECT p.product_id
     , COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
 FROM prices AS p
     LEFT JOIN unitssold AS u ON p.product_id = u.product_id
-        AND u.purchase_date BETWEEN p.start_date AND p.end_date -- 기한내에 판매된 상품
+        AND u.purchase_date BETWEEN p.start_date AND p.end_date -- 기한 내에 판매된 상품
 GROUP BY p.product_id;
