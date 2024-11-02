341c1671446e0fe4f11751537e1e2be5880f8359
Woo Yujin <biolet2001@gmail.com>
2024-11-02 16:41:55 +0900

    Add files via upload

---
+++ LeetCode/Lv.1 Easy/1251. Average Selling Price.sql
@@ -0,0 +1,7 @@
+SELECT p.product_id
+	-- If a product does not have any sold units, its average selling price is assumed to be 0.
+    , COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM prices AS p
+    LEFT JOIN unitssold AS u ON p.product_id = u.product_id
+        AND u.purchase_date BETWEEN p.start_date AND p.end_date -- 기한내에 판매된 상품
+GROUP BY p.product_id;
