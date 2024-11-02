4aaa790139f74a0dc749d83235a052a197f1f6ae
Woo Yujin <biolet2001@gmail.com>
2024-11-02 16:42:17 +0900

    Update 1251. Average Selling Price.sql

--- LeetCode/Lv.1 Easy/1251. Average Selling Price.sql
+++ LeetCode/Lv.1 Easy/1251. Average Selling Price.sql
@@ -1,5 +1,5 @@
 SELECT p.product_id
-	-- If a product does not have any sold units, its average selling price is assumed to be 0.
+    -- If a product does not have any sold units, its average selling price is assumed to be 0.
     , COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
 FROM prices AS p
     LEFT JOIN unitssold AS u ON p.product_id = u.product_id
