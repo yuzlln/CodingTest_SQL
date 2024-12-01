3c7d538a6ecd8f4591e28ce2ee639919191178a1
Woo Yujin <biolet2001@gmail.com>
2024-12-01 15:31:22 +0900

    Update 입문반 페이지를 본 세션 찾기.sql

--- solvesql/Lv.4/입문반 페이지를 본 세션 찾기.sql
+++ solvesql/Lv.4/입문반 페이지를 본 세션 찾기.sql
@@ -1,8 +1,7 @@
 WITH page AS ( -- 입문반 페이지를 본 세션
   SELECT COUNT(DISTINCT user_pseudo_id, ga_session_id) AS total
     , COUNT(DISTINCT CASE WHEN page_title = '백문이불여일타 SQL 캠프 입문반'
-                            AND event_name = 'page_view'
-                            THEN CONCAT(user_pseudo_id, ga_session_id)
+                            AND event_name = 'page_view' THEN CONCAT(user_pseudo_id, ga_session_id)
                           ELSE NULL END) AS pv_yes
   FROM ga
 )
