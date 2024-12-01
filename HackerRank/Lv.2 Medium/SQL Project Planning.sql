cb97c8b79b3aba58d8e983d30472ad6f0d7ea79e
Woo Yujin <biolet2001@gmail.com>
2024-12-01 19:54:28 +0900

    Add files via upload

---
+++ HackerRank/Lv.2 Medium/SQL Project Planning.sql
@@ -0,0 +1,14 @@
+WITH projectgroups AS (
+    SELECT *
+		    -- DATE_SUB(date, INTERVAL n DAY) : date에서 n일을 빼기
+				-- 연속된 날짜를 가진 행은 동일한 결과
+        , DATE_SUB(start_date, INTERVAL ROW_NUMBER() OVER(ORDER BY start_date) DAY) AS project_id
+    FROM projects
+)
+
+SELECT MIN(start_date) AS start_date
+    , MAX(end_date) AS end_date
+FROM projectgroups
+GROUP BY project_id
+ORDER BY DATEDIFF(MAX(end_date), MIN(start_date)) -- DATEDIFF와 같은 함수로 조합된 경우 별칭 참조가 제대로 작동하지 않을 수 있음
+    , MIN(start_date);
