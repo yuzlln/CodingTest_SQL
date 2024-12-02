9f346852472503c166b188a13cc65fbe4cf6e8d4
Woo Yujin <biolet2001@gmail.com>
2024-12-02 09:37:10 +0900

    Add files via upload

---
+++ solvesql/Lv.3/복수 국적 메달 수상한 선수 찾기.sql
@@ -0,0 +1,12 @@
+SELECT a.name
+FROM records AS r
+  INNER JOIN athletes AS a ON r.athlete_id = a.id
+  INNER JOIN games AS g ON r.game_id = g.id
+WHERE g.year >= 2000
+   AND r.medal IS NOT NULL -- 메달 수상 기록만 고려
+-- a.name으로 GROUP BY시 동일인물이 추출 될 수 있음
+-- ex. 이름은 Chen Jing으로 같지만 선수id는 다르므로 다른 선수
+-- 완전 unique한 선수id로 GROUP BY 
+GROUP BY a.id
+HAVING COUNT(DISTINCT r.team_id) >= 2 -- 2개 이상의 국적
+ORDER BY a.name;
